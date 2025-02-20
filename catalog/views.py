8b724611f0a9a4d26174e7fd7191df676480de67
maksim <m.merkulovdodo@gmail.com>
2025-02-20 18:22:49 +0300

    homework 23 done

--- catalog/views.py
+++ catalog/views.py
@@ -3,16 +3,15 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        telephone = request.POST.get('phone')
-        message = request.POST.get('message')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        telephone = request.POST.get("phone")
+        message = request.POST.get("message")
 
         return HttpResponse(f"Спасибо {name} за отзыв! Ваше сообщение получено")
 
-    return render(request, 'contacts.html')
-
+    return render(request, "contacts.html")
