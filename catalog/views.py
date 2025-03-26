7f87d89aa7a6766129b6b3f92e135bc91a991964
maksim <m.merkulovdodo@gmail.com>
2025-03-26 15:20:14 +0300

    homework 28 done

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -10,7 +11,7 @@ from django.views.generic import (
     UpdateView,
 )
 
-from catalog.forms import ProductForm
+from catalog.forms import ProductForm, ProductModeratorForm
 from catalog.models import Product, Contact
 
 
@@ -25,12 +26,31 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     template_name = "catalog/product_update.html"
     success_url = reverse_lazy("catalog:product_list")
 
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = "catalog/product_update.html"
     success_url = reverse_lazy("catalog:product_list")
+
+    def get_form_class(self):
+        """Возвращает форму в зависимости от прав пользователя."""
+        user = self.request.user
+        if user == self.object.owner:
+            return ProductForm
+        if user.has_perm("catalog.can_unpublish_product"):
+            return ProductModeratorForm
+        raise PermissionDenied
+
+    def get_queryset(self):
+        """Ограничиваем доступ только к своим товарам или для модераторов."""
+        if self.request.user.has_perm("catalog.can_unpublish_product"):
+            return Product.objects.all()
+        return Product.objects.filter(owner=self.request.user)
 
 
 class ProductDetailView(LoginRequiredMixin, DetailView):
@@ -42,6 +62,18 @@ class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = "catalog/product_delete.html"
     success_url = reverse_lazy("catalog:product_list")
+
+    def get_form_class(self):
+        user = self.request.user
+        if user.has_perm("catalog.delete_product"):
+            return ProductModeratorForm
+        raise PermissionDenied
+
+    def get_queryset(self):
+        """Ограничиваем доступ к удалению только владельцу или модератору."""
+        if self.request.user.has_perm("catalog.delete_product"):
+            return Product.objects.all()
+        return Product.objects.filter(owner=self.request.user)
 
 
 class ContactsView(LoginRequiredMixin, TemplateView):
